f829c48ad2daf6bc2a0447089ff25c6eedcace3b
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2024-01-20 23:57:55 +0400

    Add many styles for read more, post author, write logic for read more, change styles

--- src/utils/postCard.jsx
+++ src/utils/postCard.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { deletePostFromFirestore } from "../services/postServices";
-
+import userPhoto from "../images/lamp.jpg"; 
 const getRandomColor = (str) => {
   const hash = str.split("").reduce((acc, char) => char.charCodeAt(0) + acc, 1);
   const color = `hsl(${hash % 300}, 50%, 50%)`;
@@ -17,11 +17,20 @@ const PostCard = ({
 }) => {
   const [isDeleting, setIsDeleting] = useState(false);
   const backgroundColor = getRandomColor(post.userID);
+  const [isOpen, setIsOpen] = useState(false);
+  const [showReadMoreButton, setShowReadMoreButton] = useState(false);
+  const ref = useRef(null);
+
+ useEffect(()=> {
+   setShowReadMoreButton(ref.current.scrollHeight !== ref.current.clientHeight);
+ },[])
+
   const paragraphsStyles = {
     WebkitLineClamp: 3,
     WebkitBoxOrient: 'vertical',
     overflow: 'hidden',
     display: '-webkit-box',
+   
   } 
 
   const handleDelete = async () => {
@@ -46,15 +55,23 @@ const PostCard = ({
     >
       <div className="post-user">
         <div className="post-content">
+          <div className="post-author">
+        <img src={userPhoto} alt="User" className="user-photo" />
           <strong>{post.username}</strong>
           <span>
             <div className="post-date">
               <strong>Date:{date}</strong>
             </div>
           </span>
-          <p>{post.content}</p>
+          </div>
+          <p  style={isOpen ? null : paragraphsStyles} ref={ref}>{post.content}</p>
+          
         </div>
-
+        { showReadMoreButton &&
+          <button className="read-more-button" onClick={() => setIsOpen(!isOpen)}>
+{ isOpen ? 'read less...' : 'read more...' }
+          </button>
+}
         <div className="delete-button">
           {post.userID === currentUserIDForDelete && (
             <button onClick={handleDelete} disabled={isDeleting}>
